8463bf660094b026f11eba836647d8ad3d074f1a
zzl <86543402@qq.com>
2021-05-23 00:14:25 +0800

    b站历史记录-仅显示番剧

---
+++ bilibili_bangumi_history/README.md
@@ -0,0 +1,5 @@
+# Tampermonkey脚本
+添加番剧筛选选项
+
+## 效果图
+![2021-05-23-00-13-20](https://i.loli.net/2021/05/23/UQTScf4sdCXEI2g.png?x-oss-process=style/md)
---
+++ bilibili_bangumi_history/index.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         b站历史记录-仅显示番剧
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Potato-DiGua
+// @match        https://www.bilibili.com/account/history
+// @icon         http://static.hdslb.com/images/favicon.ico
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    var $ = $ || window.$;
+
+    function isBangumiHistory(liNode) {
+        if (liNode == null) {
+            return false;
+        }
+        const p = liNode.find("div.r-info.clearfix > div.cover-contain > p").first();
+        return p != null && p.text() == "番剧";
+    }
+    function setDisplay(display) {
+
+        $("#history_list > li").filter(function (_, element) {
+            return !isBangumiHistory($(element))
+        }).css("display", display ? "none" : "block");
+    }
+    function init() {
+        const input = $("<input/>", { type: "checkbox", style: "display:inline-block; vertical-align:middle; " })
+        console.log(input);
+        input.change(function () {
+            setDisplay(this.checked);
+        });
+        $("#app > div > div.newlist_info > div > div.b-head-c").after($("<div/>", {
+            style: "display:inline-block; margin-left:16px;padding:6px 0; vertical-align:middle; font-size: 12px;"
+        }).append($("<p/>").append(input, "仅显示番剧")));
+        const inputEle = input[0]
+        const resizeOb = new ResizeObserver(entries => {
+            console.log("大小发生变化");
+
+            setDisplay(inputEle.checked);
+        });
+        resizeOb.observe($("#history_list")[0])
+    }
+    // hide();
+    init();
+})();
+
