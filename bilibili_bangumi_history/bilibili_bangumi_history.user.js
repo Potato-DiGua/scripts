88d7b36adb921ee8036a21510cf666c0d88d7b8b
zzl <86543402@qq.com>
2021-09-11 13:23:07 +0800

    feature: bangumi_history增加防抖

--- bilibili_bangumi_history/bilibili_bangumi_history.user.js
+++ bilibili_bangumi_history/bilibili_bangumi_history.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         b站历史记录-仅显示番剧
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  b站历史记录添加番剧筛选功能
 // @author       Potato-DiGua
 // @supportURL   https://github.com/Potato-DiGua/scripts/blob/main/bilibili_bangumi_history/README.md
@@ -14,6 +14,22 @@
     'use strict';
     var $ = $ || window.$;
 
+    function debounce(fn, delay = 300) {
+        let ctx, args, timer = null;
+        function later() {
+            fn.apply(ctx, args);
+        }
+        return function () {
+            ctx = this;
+            args = arguments;
+            if (timer) {
+                clearTimeout(timer);
+                timer = null;
+            }
+            timer = setTimeout(later, delay);
+        }
+    }
+
     function isBangumiHistory(liNode) {
         if (liNode == null) {
             return false;
@@ -21,29 +37,31 @@
         const p = liNode.find("div.r-info.clearfix > div.cover-contain > p").first();
         return p != null && p.text() == "番剧";
     }
-    function setDisplay(display) {
 
+    function setDisplay(display) {
         $("#history_list > li").filter(function (_, element) {
             return !isBangumiHistory($(element))
         }).css("display", display ? "none" : "block");
     }
+
     function init() {
         const input = $("<input/>", { type: "checkbox", style: "display:inline-block; vertical-align:middle; " })
-        console.log(input);
         input.change(function () {
             setDisplay(this.checked);
         });
         $("#app > div > div.newlist_info > div > div.b-head-c").after($("<div/>", {
             style: "display:inline-block; margin-left:16px;padding:6px 0; vertical-align:middle; font-size: 12px;"
         }).append($("<p/>").append(input, "仅显示番剧")));
-        const inputEle = input[0]
-        const resizeOb = new ResizeObserver(entries => {
-            console.log("大小发生变化");
 
+        const inputEle = input[0];
+
+        const resizeOb = new ResizeObserver(debounce(_ => {
+            console.log("大小发生变化");
             setDisplay(inputEle.checked);
-        });
+        }));
         resizeOb.observe($("#history_list")[0])
     }
+
     init();
 })();
 
