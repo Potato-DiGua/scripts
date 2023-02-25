85a40d677d44dd38645ab431c1a17903bc0aed6f
potato-digua <86543402@qq.com>
2023-02-25 16:09:37 +0800

    build(kisshub_spider): 升级

--- kisshub_spider/rollup.config.js
+++ kisshub_spider/rollup.config.js
@@ -2,16 +2,32 @@ import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+import alias from '@rollup/plugin-alias';
 
-const babelOptions = require('./babel.config.js');
 export default {
-  input: 'src/main.tsx',
-  output: {
-    file: 'dist/bundle.js',
-    format: 'iife'
-  },
-  plugins: [resolve(),
-  commonjs(),
-  typescript(), 
-  babel({ babelHelpers: 'bundled' })]
+    input: 'src/main.tsx',
+    output: [
+        {
+            file: 'dist/bundle.js',
+            format: 'iife',
+        },
+        {
+            file: 'dist/bundle.min.js',
+            format: 'iife',
+            plugins: [terser()],
+        },
+    ],
+    plugins: [
+        alias({
+            entries: [
+                // react引入替换为dom-chef
+                // { find: 'react', replacement: 'dom-chef' }
+            ],
+        }),
+        resolve(),
+        commonjs(),
+        typescript(),
+        babel({ babelHelpers: 'bundled' }),
+    ],
 };
